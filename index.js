ac4d8160584fc5d846cb5e71c03d985703ce86af
clarkda <dusty@clarkda.com>
2014-04-27 22:37:59 -0500

    lets find everybody

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var request = new Buffer([
 
   0x00, 0x01, //HTYPE
   0x08, 0x00, //PTYPE
-  0x06, 0x04, //HLEN , PLEN
+  0x06, //HLEN
+  0x04, //PLEN
   0x00, 0x01, //OPER
 
   0xb8, 0xf6, 0xb1, 0x1c, 0x2e, 0x07, //SHA
@@ -19,15 +20,15 @@ var request = new Buffer([
   0x0a, 0x00, 0x01, 0x01  //TPA
 ]);
 
-pcap_session.inject(request);
-
 pcap_session.on('packet', function (raw_packet) {
   var packet = pcap.decode.packet(raw_packet);
-
-  if (packet.link.arp.operation == 'request') {
+  if (packet.link.arp.operation == 'request') { //opcode == 2
     return;
   }
-
-  console.log(packet.link.arp.sender_ha + " -> " + packet.link.arp.sender_pa);
-
+  console.log(packet.link.arp.sender_pa + " is " + packet.link.arp.sender_ha);
 });
+
+for(var i = 0; i <= 255; i++) {
+  request.fill(i, 41, request.length);
+  pcap_session.inject(request);
+}
