0f44fe0802d6db794b2918e1dcaf214b7732420e
clarkda <dusty@clarkda.com>
2014-04-29 00:58:35 -0500

    preference

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var arp = require('./lib/arp.js')
-  , buffer = require('buffer')
-  , ArpBuffer = require('./lib/buffer.js')
+  , arpBuffer = require('./lib/buffer.js')
   , arpSession = arp.createSession('en0');
 
-var arpBuffer = new ArpBuffer();
 arpBuffer._setMacDestination('ff:ff:ff:ff:ff:ff');
 arpBuffer._setMacSource('b8:f6:b1:1c:2e:07');
 arpBuffer._setSourceHardwareAddress('b8:f6:b1:1c:2e:07');
